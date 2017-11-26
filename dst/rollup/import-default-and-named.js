dfd37276f46922a501752d80dd2c7c7536fab0e5
Andrés Maneiro <andres.maneiro@automattic.com>
2017-11-26 21:07:38 +0100

    Add rollup example

---
+++ dst/rollup/import-default-and-named.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const constants = {
+  BAR: 1,
+  FOO: 2,
+};
+
+const foobar = 3;
+
+console.log("BAR is ", constants.BAR);
+console.log("FOO is ", constants.FOO);
+console.log("FOOBAR is ", foobar);
