b6f016d0526a4f4a883654b7582f9aebc2abd0de
Andrés Maneiro <andres.maneiro@automattic.com>
2017-12-15 12:33:02 +0100

    Add babel-minify example

---
+++ src/tree-shaking/source.babel-minify.js
@@ -0,0 +1,1 @@
+import{ONE}from'./constant-1';import{TWO}from'./constant-2';import{THREE}from'./constant-3';import{FOUR}from'./constant-4';import{FIVE}from'./constant-5';console.log('ONE is ',ONE),console.log('TWO is ',TWO),console.log('FOUR is ',FOUR),console.log('FIVE is ',FIVE);
