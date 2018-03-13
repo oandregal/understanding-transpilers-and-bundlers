3dabd2ce6aa97add7ca06d2da0f63c6cec113183
Andrés <andres.maneiro@automattic.com>
2018-03-13 20:17:42 +0100

    Move src/export/ to exports/

--- src/export/output.closure.js
+++
@@ -1,5 +0,0 @@
-// Input 0
-var module$src$export$input = {}, constants$$module$src$export$input = {BAR:1, FOO:2}, foobar$$module$src$export$input = 3, $jscompDefaultExport$$module$src$export$input = constants$$module$src$export$input;
-module$src$export$input.foobar = foobar$$module$src$export$input;
-module$src$export$input.default = $jscompDefaultExport$$module$src$export$input;
-
---
+++ src/exports/both.output.closure.js
@@ -0,0 +1,5 @@
+// Input 0
+var module$src$exports$both_input = {}, constants$$module$src$exports$both_input = {BAR:1, FOO:2}, foobar$$module$src$exports$both_input = 3, $jscompDefaultExport$$module$src$exports$both_input = constants$$module$src$exports$both_input;
+module$src$exports$both_input.foobar = foobar$$module$src$exports$both_input;
+module$src$exports$both_input.default = $jscompDefaultExport$$module$src$exports$both_input;
+
