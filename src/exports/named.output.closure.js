3ffe3bf82b3c074ae49f44edbdbc5c0713eea777
Andrés <andres.maneiro@automattic.com>
2018-03-13 20:15:27 +0100

    Move src/export-named/ to exports/

---
+++ src/exports/named.output.closure.js
@@ -0,0 +1,5 @@
+// Input 0
+var module$src$exports$named_input = {}, BAR$$module$src$exports$named_input = 1, FOO$$module$src$exports$named_input = 2;
+module$src$exports$named_input.BAR = BAR$$module$src$exports$named_input;
+module$src$exports$named_input.FOO = FOO$$module$src$exports$named_input;
+
